e55c50fc133670b91542bcc2e4d2913f35338158
Swapnil Dekate <smdekate@gmail.com>
2025-09-26 15:00:29 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      {/* testing Tailwind CSS */}
+      <h1 className="text-3xl text-center text-blue-400 italic">
+        Hello, World!
+      </h1>
+    </>
+  );
+}
+
+export default App;
